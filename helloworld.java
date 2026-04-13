32ebc9d47e2ef451807b065c97c1cff81bffe2f6
vanshika68-code <vanshikasupehia68@gmail.com>
2026-04-13 11:25:15 +0530

    helloworld

---
+++ helloworld.java
@@ -0,0 +1,6 @@
+public class helloworld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");   //ln means leave on line
+        // shoutcut to print is sout+tab
+    }
+}
