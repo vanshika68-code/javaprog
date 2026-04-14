a0e041df55109c81328e7307a67a69d3d7157114
vanshika68-code <vanshikasupehia68@gmail.com>
2026-04-14 18:55:36 +0530

    armstrome

---
+++ armstrome.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class armstrome {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+
+        System.out.print("Enter digit to find frequency: ");
+        int digit = sc.nextInt();
+
+        int count = 0;
+        int temp = num;
+        while (temp > 0) {
+            int rem = temp % 10;  
+             if (rem == digit) {
+                count++;
+            }
+            temp = temp / 10;  
+        }
+        System.out.println("Frequency of " + digit + " is: " + count);
+
+        sc.close();
+    }
+}
