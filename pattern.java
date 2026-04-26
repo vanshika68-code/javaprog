027526150417f5c4f18fa0419160e7409062efa8
vanshika68-code <vanshikasupehia68@gmail.com>
2026-04-26 21:25:07 +0530

    pattern

---
+++ pattern.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class pattern {
+    public static void main(String [] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("enter a number: ");
+        int n = sc.nextInt();
+        for (int i = 0 ; i<=n-1; i++){
+            for (int j=0;j<=2*n-2;j++){
+                if(i+j>=n-1 && i>=j-n+1){
+                    System.out.print("*");
+                }
+                else{
+                    System.out.print(" ");
+                }
+            }
+            System.out.println(" ");
+        }
+
+    }
+    
+}
