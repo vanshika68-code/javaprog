8f7656b4590273e0443494185b8905762de9f114
vanshika68-code <vanshikasupehia68@gmail.com>
2026-05-07 18:47:13 +0530

    upper_star_pattern

---
+++ upper_star_pattern.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class upper_star_pattern {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("enter a number: ");
+        int n = sc.nextInt();
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
