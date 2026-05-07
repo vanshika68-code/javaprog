e47c742bd3d3defa95fb523b878157617b155cdf
vanshika68-code <vanshikasupehia68@gmail.com>
2026-05-07 18:40:25 +0530

    square_star_pattern

---
+++ square_star_pattern.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class square_star_pattern {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("enter a number: ");
+        int n = sc.nextInt();
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
