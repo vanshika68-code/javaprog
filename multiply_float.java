eeb4dc3e327a5f1a665e53e0e3de75efb6547354
vanshika68-code <vanshikasupehia68@gmail.com>
2026-05-11 11:49:44 +0530

    multiply_float

---
+++ multiply_float.java
@@ -0,0 +1,8 @@
+public class multiply_float {
+    public static void main(String[] args) {
+        float a = 5.5f;
+        float b = 2.0f;
+        float result = a * b;
+        System.out.println("multiplication = " + result);
+    }
+}
