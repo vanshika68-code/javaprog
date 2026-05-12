2530e23d85e4d42ef418937588da248123daee74
vanshika68-code <vanshikasupehia68@gmail.com>
2026-05-12 11:54:12 +0530

    subtraction

---
+++ subtraction.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class subtraction {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter first number: ");
+        int num1 = scanner.nextInt();
+        System.out.print("Enter second number: ");
+        int num2 = scanner.nextInt();
+        int result = num1 - num2;
+        System.out.println("subtraction = " + result);
+    }
+}
