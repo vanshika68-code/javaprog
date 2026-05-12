3b347508f32dbe56eac6e40ffa0a73f33f0e9a55
vanshika68-code <vanshikasupehia68@gmail.com>
2026-05-12 23:21:51 +0530

    area_using_method

---
+++ area_using_method.java
@@ -0,0 +1,14 @@
+class rectangle{
+    int length = 10;
+    int breadth = 5;
+    void area (){
+        int result =  length * breadth;
+        System.out.println("area = " + result);
+    }
+}
+public class area_using_method {
+    public static void main(String[] args) {
+        rectangle r = new rectangle();
+        r.area();
+    }
+}
