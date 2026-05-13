e2f5166d39a2a18630fbbc346785bc694aaa928e
vanshika68-code <vanshikasupehia68@gmail.com>
2026-05-13 23:51:07 +0530

    tableof2

---
+++ tableof2.java
@@ -0,0 +1,13 @@
+public class tableof2 {
+    public static void main(String[] args) {
+        int num = 2 ;
+        int multi;
+        
+        for (int i = 1 ; i <= 10 ; i++){
+            multi = num*i;
+            System.out.println(num + " * " + i + " = " + multi);
+
+        }
+    }
+}
+
