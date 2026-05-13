926a613af1c30cfa6e22f5b24645cf28c17d46a9
vanshika68-code <vanshikasupehia68@gmail.com>
2026-05-13 23:42:39 +0530

    vehicle_interface

---
+++ vehicle_interface.java
@@ -0,0 +1,23 @@
+interface vehicle{
+    void start();
+}
+class car implements vehicle{
+    public void start(){
+        System.out.println("car starts with key.");
+    }
+}  
+class scooty implements vehicle{
+    public void start(){
+        System.out.println("scooty starts with kick.");
+    }
+}
+public class vehicle_interface {
+    public static void main(String[] args){
+        car c = new car();
+        c.start();
+        scooty s = new scooty();
+        s.start();
+    }
+    
+}
+
