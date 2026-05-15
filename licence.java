59c3af3ca9e9d2c70b575c1b29e72b4e39f98d59
vanshika68-code <vanshikasupehia68@gmail.com>
2026-05-15 14:13:04 +0530

    licence

---
+++ licence.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class licence {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in) ;
+        System.out.print("enter your age :");
+        int age = sc.nextInt();
+        if (age>=18){
+            System.out.println("person is eligible to get driving licence");
+        }
+        else{
+            System.out.println("person is not eligible to get driving licence");
+        }
+    }
+}
