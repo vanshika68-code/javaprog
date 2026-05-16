aaf60fc6eb12ff929a4eecaacbe2e451a17dc008
vanshika68-code <vanshikasupehia68@gmail.com>
2026-05-16 19:09:28 +0530

    bank_set_get

---
+++ bank_set_get.java
@@ -0,0 +1,17 @@
+class bank{
+    private int balance;
+    public void setbalance(int b){
+        balance = b;
+    }
+    public int getbalance(){
+        return balance;
+    }
+}
+public class bank_set_get {
+    public static void main(String[] args) {
+        bank bank1 = new bank();
+        bank1.setbalance(1000);
+        System.out.println("balance: " + bank1.getbalance());
+    }
+    
+}
